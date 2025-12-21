937de367bb27708781a21c0968fe765d7a4dab9d
David-Vanuxin <2468steed@gmai.com>
2025-12-21 19:13:17 +0300

    Написаны тесты для /sign-in, /log-in, /user/id

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request } from "express"
 
 const app = express()
 const port = 3000
@@ -7,15 +7,56 @@ app.use(express.json())
 
 app.use((req, res, next) => {
   res.set({
-    'Access-Control-Allow-Origin': "*",
-    'Access-Control-Allow-Methods': "*",
-    'Access-Control-Allow-Headers': "*",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers": "*",
   })
   next()
 })
 
-app.get('/', (req, res) => {
-  res.end('Hello, World!')
+app.post("/sign-up", (req, res) => {
+  res.json({ status: "OK" })
+})
+
+app.post("/log-in", (req, res) => {
+  res.json({ token: Math.round(Math.random() * 1e12) })
+})
+
+const trueToken = 123456789
+
+function userHasAccess(req: Request) {
+  const authHeader = req.get("Authorization")
+
+  if (authHeader === undefined) return false
+
+  const token = authHeader.split(" ")[1]
+
+  if (token !== trueToken.toString()) return false
+
+  return true
+}
+
+app.get("/user/:id", (req, res) => {
+  if (!userHasAccess(req)) {
+    res.status(400)
+    res.json({ error: "Access denied" })
+    return
+  } 
+
+  const user = {
+    id: req.params.id,
+    firstname: "Иванов",
+    secondname: "Иван",
+    patronymic: "Иванович",
+    email: "test@email.com",
+    password: "1234",
+    birthdate: Date.now(),
+  }
+  res.json(user)
+})
+
+app.get("/user/", (req, res) => {
+  res.json({ status: "in development" })
 })
 
 app.listen(port, () => {
