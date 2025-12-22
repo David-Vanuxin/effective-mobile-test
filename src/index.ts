67abebe02b6d103569b6abd2babf39ea4c2656cc
David-Vanuxin <2468steed@gmai.com>
2025-12-22 19:55:13 +0300

    Добавлен обработчик ошибок на уровне приложения

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request } from "express"
+import express, { Request, Response, NextFunction } from "express"
 import "reflect-metadata"
 import { AppDataSource } from "./data-source.js"
 import User from "./entity/User.js"
@@ -40,6 +40,11 @@ app.post("/purge", async (req, res) => {
 
 app.use((req, res, next) => {
   res.status(404).send("Invalid path")
+})
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err)
+  res.status(500).send('Server error')
 })
 
 app.listen(port, () => {
