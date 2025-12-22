f8755d10b0b0fa7f7f187f6f30fc1a612f30c5d2
David-Vanuxin <2468steed@gmai.com>
2025-12-22 19:23:24 +0300

    Добавлен обработчик для неверных url

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,10 @@ app.post("/purge", async (req, res) => {
   return res.json({ status: "purged" })
 })
 
+app.use((req, res, next) => {
+  res.status(404).send("Invalid path")
+})
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
